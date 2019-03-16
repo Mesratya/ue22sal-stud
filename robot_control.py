efe8e8571779c0e410e6148787b166aaf1e9d059
Mesratya <48388826+Mesratya@users.noreply.github.com>
2019-03-16 19:18:03 +0100

    Add files via upload

---
+++ robot_control.py
@@ -0,0 +1,227 @@
+import numpy as np
+import time
+import random
+import sonar_filter
+
+class RobotControl:
+    def __init__(self):
+        self.distBetweenWheels = 0.12
+        self.nTicksPerRevol = 720
+        self.wheelDiameter = 0.06
+
+    def testMove(self,rb,speedLeft,speedRight,duration):
+        # forward motion
+        rb.set_speed(speedLeft,speedRight)
+        loopIterTime = 0.050
+        tStart = time.time()
+        while time.time()-tStart < duration:
+            time.sleep(loopIterTime) # wait 
+        # stop the robot 
+        rb.stop()
+
+    def testInfiniteObstacle (self,rb):
+        loopIterTime = 0.050  # duration of loop iteration 50 ms
+        legTimeMax = 5.0 # always turn after 5s (even if no obsctacle)
+        distObstacle = 0.3 # stops and change direction if obstacle 
+                           # at less than 30 cm
+        while True:  # infinite loop : stop by typing ctrl-C
+            rb.set_speed(90,90)
+            tStartLeg = time.time()
+            while True:
+                t0 = time.time()
+                if time.time()-tStartLeg >= legTimeMax:
+                    break
+                distFront = rb.get_sonar("front")
+                #print ("distFront :",distFront)
+                if distFront != 0.0 and distFront < distObstacle:
+                    break
+                t1 = time.time()
+                dt = loopIterTime - (t1-t0)
+                if dt>0:
+                    time.sleep(dt) # wait for end of iteration
+            # in the case the robot is trapped in front of a wall
+            # go back at speed 40 for 0.5 seconds 
+            rb.set_speed(-40,-40)
+            time.sleep (0.5)
+            # set random orientation by setting rotation duration
+            # minimum time is 0.2 seconds, max is 1.5 seconds
+            rotationTime = 0.2+1.3*random.random()
+            rotationDirection = random.random()
+            if rotationDirection < 0.5:
+                self.testMove(rb,40,-40,rotationTime)
+            else:
+                self.testMove(rb,-40,40,rotationTime)
+
+    def inPlaceTurnRight(self, rb, ang):   # ang in degrees  
+        setPoint =  (self.nTicksPerRevol/360) * (ang*self.distBetweenWheels/self.wheelDiameter)
+        loopIterationTime = 0.05
+        odoLeft, odoRight = rb.get_odometers()
+        while True :
+            t0 = time.time()
+            nTicksDone = rb.get_odometers()
+            nTicksDone = nTicksDone[0] - odoLeft
+            ControlError = setPoint-nTicksDone
+
+            if abs(ControlError) < 2.5 :
+                rb.stop()
+                break
+            rb.set_speed(5,-5)
+            execTime = time.time() - t0
+            deltaTime = loopIterationTime - execTime
+            if deltaTime > 0 :
+                time.sleep(deltaTime)
+
+
+
+    def inPlaceTurnLeft(self, rb, ang):  # ang in degrees  
+        setPoint = (self.nTicksPerRevol / 360) * (ang * self.distBetweenWheels / self.wheelDiameter)
+        loopIterationTime = 0.05
+        odoLeft, odoRight = rb.get_odometers()
+        while True:
+            t0 = time.time()
+            nTicksDone = rb.get_odometers()
+            nTicksDone = nTicksDone[1] - odoRight
+            ControlError = setPoint - nTicksDone
+            if abs(ControlError) < 2.5:
+                rb.stop()
+                break
+            rb.set_speed(-5, 5) # -5 et 5 ça marche bien
+            execTime = time.time() - t0
+            deltaTime = loopIterationTime - execTime
+            if deltaTime > 0:
+                time.sleep(deltaTime)
+
+    def goLineOdometer(self, rb, dist, speed):  
+        # dist in meter
+        # speed in % (0 to 100)
+        setPoint = (self.nTicksPerRevol / np.pi) * (dist/self.wheelDiameter)
+        loopIterationTime = 0.05
+        odoLeft, odoRight = rb.get_odometers()
+        while True:
+            t0 = time.time()
+            nTicksDone = rb.get_odometers()
+            nTicksDone = nTicksDone[1] - odoRight
+            #ControlError = setPoint - nTicksDone
+            if nTicksDone > setPoint and dist > 0:
+                rb.stop()
+                break
+            if nTicksDone < setPoint and dist < 0:
+                rb.stop()
+                break
+            if dist > 0 :
+                rb.set_speed(speed, speed)
+            if dist < 0 :
+                rb.set_speed(-speed, -speed)
+            execTime = time.time() - t0
+            deltaTime = loopIterationTime - execTime
+
+            if deltaTime > 0:
+                time.sleep(deltaTime)
+
+    def followTheLeftWall(self,rb,dist,speed):
+        setPoint = dist
+        nominalSpeed = speed
+        derivOk = False
+        fltFront = sonar_filter.SonarFilter()
+        fltFront.set_iir_a(0.7)
+        fltFront.reset_iir()
+        fltLeft = sonar_filter.SonarFilter()
+        fltLeft.set_iir_a(0.7)
+        fltLeft.reset_iir()
+        kp = 10 # 30 et 50 /10 et 1000 / 4 et 4.4
+        kd = 1000
+        deltaSpeedMax = 10
+        distObstacle = 0.5
+        loopIterationTime = 0.05
+        while True:
+            t0 = time.time()
+
+            distWall_raw = rb.get_sonar("left")
+            distWall = fltLeft.median_filter(distWall_raw)
+            distWall = fltLeft.iir_filter(distWall)
+            ControlError_Left = setPoint - distWall
+            if derivOk :
+                derivError = ControlError_Left - lastError
+                deltaSpeed = kp * ControlError_Left + kd * derivError
+            else :
+                deltaSpeed = kp * ControlError_Left
+            #print("deltaSpeed = {0}".format(deltaSpeed))
+            if deltaSpeed > deltaSpeedMax:
+                deltaSpeed = deltaSpeedMax
+            if deltaSpeed < - deltaSpeedMax:
+                deltaSpeed = - deltaSpeedMax
+
+            rb.set_speed(nominalSpeed + deltaSpeed, nominalSpeed - deltaSpeed)
+            lastError = ControlError_Left
+            derivOk = True
+
+            distFront_raw = rb.get_sonar("front")
+            distFront = fltFront.median_filter(distFront_raw)
+            distFront = fltFront.iir_filter(distFront)
+            #print(distFront_raw,distFront,distObstacle)
+            ControlError_Front = distObstacle - distFront
+            if distFront_raw != 0.0 and ControlError_Front > 0 :
+                rb.stop()
+                break
+
+            execTime = time.time() - t0
+            deltaTime = loopIterationTime - execTime
+
+            if deltaTime > 0:
+                time.sleep(deltaTime)
+
+
+    def followTheRightWall(self,rb,dist,speed,max_time = 120):
+        setPoint = dist
+        nominalSpeed = speed
+        derivOk = False
+        fltFront = sonar_filter.SonarFilter()
+        fltFront.set_iir_a(0.7)
+        fltFront.reset_iir()
+        fltRight = sonar_filter.SonarFilter()
+        fltRight.set_iir_a(0.7)
+        fltRight.reset_iir()
+        kp = 4.4
+        kd = 4
+        deltaSpeedMax = 10
+        distObstacle = 0.5
+        loopIterationTime = 0.05
+        global_t0 = time.time()
+        while True:
+            t0 = time.time()
+
+            distWall_raw = rb.get_sonar("right")
+            distWall = fltRight.median_filter(distWall_raw)
+            distWall = fltRight.iir_filter(distWall)
+            ControlError_Right = setPoint - distWall
+            if derivOk :
+                derivError = ControlError_Right - lastError
+                deltaSpeed = kp * ControlError_Right + kd * derivError
+            else :
+                deltaSpeed = kp * ControlError_Right
+            #print("deltaSpeed = {0}".format(deltaSpeed))
+            if deltaSpeed > deltaSpeedMax:
+                deltaSpeed = deltaSpeedMax
+            if deltaSpeed < - deltaSpeedMax:
+                deltaSpeed = - deltaSpeedMax
+
+            rb.set_speed(nominalSpeed - deltaSpeed, nominalSpeed + deltaSpeed)
+            lastError = ControlError_Right
+            derivOk = True
+
+            distFront_raw = rb.get_sonar("front")
+            distFront = fltFront.median_filter(distFront_raw)
+            distFront = fltFront.iir_filter(distFront)
+            #print(distFront_raw,distFront,distObstacle)
+            ControlError_Front = distObstacle - distFront
+            global_time = time.time() - global_t0
+            print(global_time)
+            if (distFront_raw != 0.0 and ControlError_Front > 0) or (distWall_raw == 0.0) or (global_time > max_time) :
+                rb.stop()
+                break
+
+            execTime = time.time() - t0
+            deltaTime = loopIterationTime - execTime
+
+            if deltaTime > 0:
+                time.sleep(deltaTime)
---
+++ robot_mission.py
@@ -0,0 +1,74 @@
+import rob1a_v01 as rob1a  # get robot simulator
+import robot_control  # get robot control functions 
+import sonar_filter # low pass filters
+import numpy as np
+import time
+
+# the best approach is to place all your functions in robot_control.py
+# however, if you need additionnal functions, you have to put them here
+# ...
+
+if __name__ == "__main__":
+    rb = rob1a.Rob1A()   # create a robot (instance of Rob1A class)
+    ctrl = robot_control.RobotControl() # create a robot controller
+
+    # put here your code to solve the challenge
+    speed = 60 # 40 ça marcjait nickel
+    dist = 0.5
+    ctrl.goLineOdometer(rb, dist, speed)
+    angle = 90.0
+    ctrl.inPlaceTurnRight(rb, angle)
+    dist = 0.5
+    ctrl.goLineOdometer(rb, dist, speed)
+    angle = 90.0
+    ctrl.inPlaceTurnLeft(rb, angle)
+    dist = 1.5
+    ctrl.goLineOdometer(rb, dist, speed)
+    ctrl.inPlaceTurnRight(rb, angle)
+    ctrl.followTheLeftWall(rb,0.5,speed)
+    ctrl.inPlaceTurnRight(rb, angle)
+    ctrl.followTheLeftWall(rb,0.5,speed)
+
+    speed = 40
+
+    ctrl.inPlaceTurnRight(rb, angle)
+    ctrl.goLineOdometer(rb,0.55, speed)
+    ctrl.inPlaceTurnRight(rb, 25)
+
+    speed = 15 # ne pas toucher à cette vitesse elle joue sur le temps de parcourt des murs en v !
+
+    ctrl.followTheRightWall(rb,0.45,speed,40)
+    ctrl.inPlaceTurnLeft(rb,20)
+    ctrl.followTheRightWall(rb,0.45,speed,5)
+    ctrl.inPlaceTurnLeft(rb,15)
+    ctrl.followTheRightWall(rb,0.45,speed)
+
+
+    ctrl.goLineOdometer(rb,0.1,speed)
+    ctrl.followTheLeftWall(rb,0.5,speed)
+
+    ctrl.inPlaceTurnRight(rb,80)
+    ctrl.goLineOdometer(rb,0.7,speed)
+    print("je suit le 2nd mur")
+    ctrl.inPlaceTurnRight(rb,20)
+
+    ctrl.followTheRightWall(rb, 0.45, speed, 40)
+    ctrl.inPlaceTurnLeft(rb, 20)
+    ctrl.followTheRightWall(rb, 0.45, speed, 5)
+    ctrl.inPlaceTurnLeft(rb, 15)
+    ctrl.followTheRightWall(rb, 0.45, speed)
+    
+
+    ctrl.followTheLeftWall(rb,0.5,speed)
+
+
+
+
+
+
+
+
+    # safe end : stop the robot, then stop the simulator
+    rb.stop()
+    rb.log_file_off() # end log
+    rb.full_end()
